670526ded7e80f18ccfae89af2196fb0cba304a4
Emma Litvin <guslitvem@student.gu.se>
2023-11-22 12:35:12 +0100

    #1 Fix database schema

--- asl_translator_gui/website/models.py
+++ asl_translator_gui/website/models.py
@@ -3,9 +3,9 @@ import datetime
     
 # Model training
 class Training(models.Model):
-    model_id = models.AutoField(primary_key=True)
-    training_date = models.DateField(default=datetime.datetime.today)
-    accuracy = models.DecimalField(default=0, decimal_places=2, max_digits=5)
+    modelid = models.AutoField(primary_key=True)
+    trainingdate = models.DateField(default=datetime.datetime.today)
+    accuracy = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
-        return self.model_id
+        return str(self.modelid)
