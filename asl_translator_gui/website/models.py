3110845f0fafab783ac4c36d195fa71a27a59dd5
Emma Litvin <guslitvem@student.gu.se>
2023-11-24 11:02:08 +0100

    #1 Change training  model to include model weights

--- asl_translator_gui/website/models.py
+++ asl_translator_gui/website/models.py
@@ -6,6 +6,8 @@ class Training(models.Model):
     modelid = models.AutoField(primary_key=True)
     trainingdate = models.DateField(default=datetime.datetime.today)
     accuracy = models.DecimalField(max_digits=5, decimal_places=2)
+    modelweights = models.FileField(upload_to="media/models/", null=True)
+    isdeployed = models.BooleanField(default=False)
 
     class Meta:
         ordering = ('-accuracy',)
