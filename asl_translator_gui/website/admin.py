68bc956f14788d62047c0f74f76858b71420fbc1
Emma Litvin <guslitvem@student.gu.se>
2023-12-06 09:37:44 +0100

    #1 Add new table to django admin site

--- asl_translator_gui/website/admin.py
+++ asl_translator_gui/website/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.register(Training_input)
 admin.site.register(Training)
 admin.site.register(Translation_input)
 admin.site.register(Translation_output)
