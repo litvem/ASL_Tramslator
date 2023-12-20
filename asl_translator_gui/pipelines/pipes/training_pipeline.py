14ba9ef623dfc0cd7f9d4a9a8d2e84560a13c1d8
aminmah <aminmah@student.chalmers.se>
2023-12-20 16:47:28 +0100

    update retraining

--- asl_translator_gui/pipelines/pipes/training_pipeline.py
+++ asl_translator_gui/pipelines/pipes/training_pipeline.py
@@ -10,18 +10,16 @@ from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.utils import to_categorical
 from sklearn.metrics import accuracy_score
 from sklearn.base import BaseEstimator, TransformerMixin
-from joblib import dump, load
 import json
 from website.models import *
 from data.upload_retraining_json_to_db import DataHandler
-from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
+from sklearn.metrics import accuracy_score
 import tensorflow as tf
-
+import shutil
 
 
 #path to our np arrays
 DATA_PATH = os.path.join(os.path.abspath('data/MP_Data')) 
-DATA_PATH_TEST = os.path.join(os.path.abspath('data/test_set')) 
 #list of all our labels
 actions = np.array([
     'nice',
@@ -42,28 +40,11 @@ actions = np.array([
                    ])
 directory_path = os.path.abspath("media/input")
 
-# path_to_model_weights = None
-# Last_deployed_model = Training.objects.get(is_deployed = True)
-# path_to_model_weights = Last_deployed_model.model_weights
-# print(path_to_model_weights)
-
-# json_files = [f for f in os.listdir(directory_path) if f.endswith('.json')]
-# if json_files:
-#     # Get the full paths of the JSON files
-#     json_paths = [os.path.join(directory_path, f) for f in json_files]
-
-#     # Find the newest JSON file based on modification time
-#     newest_json_path = max(json_paths, key=os.path.getmtime)
-# last_uploaded_json_file = json.loads((Training_input.objects.latest('id').tr_input_file).read().decode('utf-8'))
-# clean_texts = [entry.get("clean_text", "") for entry in last_uploaded_json_file]
-# actions= np.array(clean_texts)
-# print(actions)
-
 
 #class for the model trainer
 class TrainModelTransformer(BaseEstimator, TransformerMixin):
     #constructor for the class, gets the number of epochs, number of frames to include and the list of lebels
-    def __init__(self, actions,  target_file_count=30, epochs=1000):
+    def __init__(self, actions,  target_file_count=30, epochs=55):
         self.target_file_count = target_file_count
         self.epochs = epochs
         self.target_file_count = target_file_count
@@ -89,87 +70,60 @@ class TrainModelTransformer(BaseEstimator, TransformerMixin):
         print(actions_retrain)
         
         self.actions = actions_retrain
+
+        data_addresses = ["data/MP_Data", "data/MP_Data_original"]
+
+        for address in data_addresses:
+            for action in actions_retrain:
+                videos = os.listdir(os.path.abspath(address) + "/" + "{}".format(action))
+                if ".DS_Store" in videos:
+                    videos.remove(".DS_Store")
+                #pick 2 random videos for each action
+                #random_videos = random.sample(videos, 1)
+                #for all those videos
+                for sequence in videos:
+                    window = []
+                    if sequence != ".DS_Store":
+                        #count the number of np arrays (frames) this video has
+                        number_of_f = os.listdir(os.path.abspath(address) + "/" + "{}".format(action) + "/" + sequence)
+                        f_size = len(number_of_f)
+
+                        #if the number of frames is smaller/equal 29, get all the 29 frames and add them to the windows list
+                        if f_size < 27:
+                            continue
+                        elif f_size == self.target_file_count:
+                            for frame_num in range(3, self.target_file_count):
+                                res = np.load(
+                                    os.path.join(os.path.join(os.path.abspath(address)), action, sequence, "{}.npy".format(frame_num)))
+                                window.append(res)
+                        #if the number of frames is 60, get every other frame 
+                        elif f_size >= 60:
+                            for frame_num in range(3, f_size - 4, 2):
+                                res = np.load(
+                                    os.path.join(os.path.join(os.path.abspath(address)), action, sequence, "{}.npy".format(frame_num)))
+                                window.append(res)
+                        #if the number of frames is between 29 and 60, pick 30 frames out of all available
+                        else:
+                            random_numbers = random.sample(range(3, f_size), self.target_file_count)
+                            random_numbers.sort()
+                            for frame_num in random_numbers:
+                                res = np.load(
+                                    os.path.join(os.path.join(os.path.abspath(address)), action, sequence, "{}.npy".format(frame_num)))    
+                                window.append(res)
+
+                        sequences.append(window)
+                        labels.append(label_map[action])
+
         
-        for action in actions_retrain:
-            videos = os.listdir(os.path.abspath("data/MP_Data") + "/" + "{}".format(action))
-            if ".DS_Store" in videos:
-                videos.remove(".DS_Store")
-            #pick 2 random videos for each action
-            random_videos = random.sample(videos, 1)
-            #for all those videos
-            for sequence in random_videos:
-                window = []
-                if sequence != ".DS_Store":
-                    #count the number of np arrays (frames) this video has
-                    number_of_f = os.listdir(os.path.abspath("data/MP_Data") + "/" + "{}".format(action) + "/" + sequence)
-                    f_size = len(number_of_f)
-
-                    #if the number of frames is smaller/equal 29, get all the 29 frames and add them to the windows list
-                    if f_size < 27:
-                        continue
-                    elif f_size == self.target_file_count:
-                        for frame_num in range(3, self.target_file_count):
-                            res = np.load(
-                                os.path.join(DATA_PATH, action, sequence, "{}.npy".format(frame_num)))
-                            window.append(res)
-                    #if the number of frames is 60, get every other frame 
-                    elif f_size >= 60:
-                        for frame_num in range(3, f_size - 4, 2):
-                            res = np.load(
-                                os.path.join(DATA_PATH, action, sequence, "{}.npy".format(frame_num)))
-                            window.append(res)
-                    #if the number of frames is between 29 and 60, pick 30 frames out of all available
-                    else:
-                        random_numbers = random.sample(range(3, f_size), self.target_file_count)
-                        random_numbers.sort()
-                        for frame_num in random_numbers:
-                            res = np.load(
-                                os.path.join(DATA_PATH, action, sequence, "{}.npy".format(frame_num)))    
-                            window.append(res)
-
-                    sequences.append(window)
-                    labels.append(label_map[action])
-
-        
-
-
-        sequences_test, labels_test = [], []
-        for action_test in self.actions:
-            videos = os.listdir(os.path.abspath("data/test_set") + "/" + "{}".format(action_test))
-            if ".DS_Store" in videos:
-                videos.remove(".DS_Store")
-            for sequence in videos:
-                window = []
-                if sequence != ".DS_Store":
-                    #count the number of np arrays (frames) this video has
-                    number_of_f = os.listdir(os.path.abspath("data/test_set") + "/" + "{}".format(action_test) + "/" + sequence)
-                    f_size = len(number_of_f)
-
-                    if f_size == self.target_file_count:
-                        for frame_num in range(3, self.target_file_count):
-                            res = np.load(
-                                os.path.join(DATA_PATH_TEST, action_test, sequence, "{}.npy".format(frame_num)))
-                            window.append(res)
-                    
-                    sequences_test.append(window)
-                    labels_test.append(label_map[action_test])
-
-    
-        print(len(labels_test))
         #make a np array of all the sequences
         Z = np.array(sequences)
         #make the labels catagorical
         y = to_categorical(labels).astype(int)
-        #split the data into train and test set with 80-20% ratio
-        X_train, X_test, y_train, y_test = train_test_split(Z, y, test_size=0.2)
-
-        K = np.array(sequences_test)
-        P = to_categorical(labels_test).astype(int)
-        _, X_test_set, _, y_test_set = train_test_split(K, P, test_size=0.99)
-        print(X_test_set.shape)
-        print(y_test_set.shape)
-        print(label_map)
+        #split the data into train and test set with 90-10% ratio
+        X_train, X_test, y_train, y_test = train_test_split(Z, y, test_size=0.1, stratify=y)
         
+        print(X_train.shape)
+
         #model's architecture
         self.model = Sequential()
         self.model.add(LSTM(64, return_sequences=True, activation='tanh', input_shape=(27,1662)))
@@ -182,27 +136,13 @@ class TrainModelTransformer(BaseEstimator, TransformerMixin):
 
         #compile the model and fit
         self.model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-        
-        
-        Last_deployed_model = Training.objects.get(is_deployed = True)
-        path_to_model_weights = str(Last_deployed_model.model_weights)
-        print(path_to_model_weights)
-        abs_path_to_model_weights = os.path.abspath("media/"+ path_to_model_weights)
-        print(abs_path_to_model_weights)
-        # path_to_model_weights = "C:/Users/yasi7/Downloads/data/gui_new/ASL-translator/asl_translator_gui/media/models/actions_solution_20l_v0_1.h5"
-        
-        #self.model.load_weights(abs_path_to_model_weights)
-        self.model = tf.keras.models.load_model(os.path.abspath("media/models/"+ "MyModel_tf"))
-
-        _, goz = self.model.evaluate(X_test_set, y_test_set)
-        print('goz: ', goz)
 
         self.model.fit(X_train, y_train, epochs=self.epochs, callbacks=[tb_callback])
         
 
-        yhat = self.model.predict(X_test_set)
+        yhat = self.model.predict(X_test)
 
-        ytrue = np.argmax(y_test_set, axis=1).tolist()
+        ytrue = np.argmax(y_test, axis=1).tolist()
         yhat = np.argmax(yhat, axis=1).tolist()
 
         
@@ -211,7 +151,8 @@ class TrainModelTransformer(BaseEstimator, TransformerMixin):
 
         # Evaluate the model on the test set
         self.accuracy = accuracy_score(ytrue, yhat)
-        #self.accuracy = self.train_accuracy - 0.05
+
+    
         print("Model Accuracy on test set:", self.accuracy)
 
         print("Model Accuracy on train set:", self.train_accuracy)
@@ -220,36 +161,28 @@ class TrainModelTransformer(BaseEstimator, TransformerMixin):
     def transform(self, X):
         #return the results of the pipeline
         path_to_MP_Data = os.path.join(os.path.abspath("data/MP_Data"))
+        actions_in_MP_Data = os.listdir(path_to_MP_Data)
+        for action in actions_in_MP_Data:
+            try:
+                shutil.rmtree(os.path.join(path_to_MP_Data, action))
+                print(f"Deleted: {action}")
+            except Exception as e:
+                print(f"Error deleting {action}: {e}")
+
+
         path_to_videos = os.path.join(os.path.abspath("data/videos"))
-        files_in_MP_Data = os.listdir(path_to_MP_Data)
-        files_in_videos = os.listdir(path_to_videos)
-        for file_name in files_in_MP_Data:
-            file_path = os.path.join(path_to_MP_Data, file_name)
+        actions_in_videos = os.listdir(path_to_videos)
+        for action in actions_in_videos:
             try:
-                os.rmdir(file_path)
-                print(f"Deleted: {file_path}")
+                shutil.rmtree(os.path.join(path_to_videos, action))
+                print(f"Deleted: {action}")
             except Exception as e:
-                print(f"Error deleting {file_path}: {e}")
-        for file_name in files_in_videos:
-            file_path = os.path.join(path_to_MP_Data, file_name)
-            try:
-                os.rmdir(file_path)
-                print(f"Deleted: {file_path}")
-            except Exception as e:
-                print(f"Error deleting {file_path}: {e}")
+                print(f"Error deleting {action}: {e}")
+
         return {'model': self.model, 'accuracy': self.accuracy, 'train_accuracy': self.train_accuracy}
 
 
 
 train_pipeline = Pipeline([
-    ('preprocessAndFit', TrainModelTransformer(actions=actions,target_file_count=30, epochs=1000))
+    ('preprocessAndFit', TrainModelTransformer(actions=actions,target_file_count=30, epochs=55))
 ])
-
-#runs the pipeline ****for testing, comment later***** 
-# result = train_pipeline.fit_transform(None)
-# trained_model = result['model']
-# accuracy = result['accuracy']
-# train_accuracy = result['train_accuracy']
-
-# #save the model
-# dump(trained_model, 'asl_translator_gui/trained_models/{}.joblib'.format(random.randint(1000, 9999)))
